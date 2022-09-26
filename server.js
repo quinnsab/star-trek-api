2c27d82ade7fb01d90b2f265b479a8b2776a455e
Erin Sheridan <erinsabs@gmail.com>
2022-09-26 18:46:20 -0400

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const { response, request } = require('express')
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const cors = require ('cors')
 const PORT = 8000
 
 app.use(cors())
@@ -63,6 +64,24 @@ const aliens = {
         'image': 'https://static.wikia.nocookie.net/aliens/images/4/42/EzriDax.jpg'
     }
 }
+
+// app.get('/', (req, res) => {
+//     response.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/api/:alienName', (req, res) => { 
+//     const aliensName = request.params.alienName.toLowerCase()
+//     if(aliens[aliensName]){
+//         response.json(aliens[aliensName])  
+//     } else {
+//         response.json(aliens['humans'])
+//     }
+// })
+
+// app.listen(process.env.PORT || PORT, () => {
+//     console.log('server is running, you better catch it')
+// })
+
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
